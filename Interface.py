ed4a88cf9500e9c489486cdc5e56fe87b355a5ed
Gabriel Pagani <163437299+gpganis@users.noreply.github.com>
2024-10-11 11:07:33 -0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -4,9 +4,9 @@ from Funcoes import Verificar_Diretorio
 def main(page: ft.page):
     page.title = "Automação"
     page.window.width = 605
-    page.window.height = 650
-    page.window.center()
-    page.window.to_front()
+    page.window.height = 390
+    #page.window.center()
+    #page.window.to_front()
     page.window.resizable = False
     page.window.maximizable = False
     page.bgcolor = ft.colors.WHITE
@@ -16,25 +16,25 @@ def main(page: ft.page):
         
         if txt1.value == '':
             txt1.error_text = "Campo Obrigatório"
-            page.window.height = 675
+            page.window.height = 415
         elif txt1.value.isnumeric() == False:
             txt1.error_text = "Digite Somente números"
-            page.window.height = 675
+            page.window.height = 415
         elif len(txt1.value) != 5:
             txt1.error_text = "O Código deve ter 5 digitos"
-            page.window.height = 675
+            page.window.height = 415
         else:
             txt1.error_text = None
 
         if txt2.value == '':
             txt2.error_text = "Campo Obrigatório"
-            page.window.height = 675
+            page.window.height = 415
         elif txt2.value.isnumeric() == False:
             txt2.error_text = "Digite Somente números"
-            page.window.height = 675
+            page.window.height = 415
         elif len(txt2.value) != 5:
             txt2.error_text = "O Código deve ter 5 digitos"
-            page.window.height = 675
+            page.window.height = 415
         else:           
             txt2.error_text = None
         
@@ -42,7 +42,7 @@ def main(page: ft.page):
 
         if ((txt1.value != '' and txt1.value.isnumeric() == True and len(txt1.value) == 5) and 
               (txt2.value != '' and txt2.value.isnumeric() == True and len(txt2.value) == 5)):          
-            page.window.height = 650 
+            page.window.height = 390 
             txt1.disabled = True
             txt2.disabled = True
             btn1.disabled = True
@@ -61,7 +61,7 @@ def main(page: ft.page):
 
     txt1 = ft.TextField(label="Último Código Fornecedor", width=240)
     txt2 = ft.TextField(label="Último Código Cliente", width=240)
-    txt3 = ft.TextField(value=" ", label="Output", width=500, read_only=True, bgcolor=ft.colors.GREY_200, multiline=True, min_lines=12, max_lines=12)
+    txt3 = ft.TextField(value=" ", label="Output", width=500, read_only=True, bgcolor=ft.colors.GREY_200)
 
     btn1 = ft.ElevatedButton(tooltip='Inicia o Programar',text="Iniciar",width=435,height=50,bgcolor=ft.colors.BLUE_900,color=ft.colors.WHITE,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5)),on_click=btn_clicked)
     btn2 = ft.ElevatedButton(tooltip='Finaliza o Programar',text="Encerrar",width=500,height=50,bgcolor=ft.colors.BLUE_900,color=ft.colors.WHITE,style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5)),on_click=Close, disabled=True)
