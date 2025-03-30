89e08f369ac480549bb8668c84c0272b306144ae
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-03-30 19:34:09 -0300

    Update app

--- src/view/app.py
+++ src/view/app.py
@@ -1,4 +1,6 @@
 import flet as ft
+from time import sleep
+from src.utils.connection import server_request
 
 
 class App:
@@ -20,6 +22,29 @@ class App:
         self.page.update()
 
     def show_interface(self) -> None:
+        def search(e):
+            response = server_request(
+                query="""
+                    SELECT
+                        (SELECT TOP 1 CODCFO FROM FCFO WHERE CODCFO LIKE 'F%' and CODCOLIGADA = 5 ORDER BY DATACRIACAO DESC, CODCFO DESC) AS COD_FOR,
+                        (SELECT TOP 1 CODCFO FROM FCFO WHERE CODCFO LIKE 'C%' and CODCOLIGADA = 5 ORDER BY DATACRIACAO DESC, CODCFO DESC) AS COD_CLI;
+                """
+            )
+            supplier_code.value = response['data'][0]['COD_FOR']
+            client_code.value = response['data'][0]['COD_CLI']
+
+        def start(e):
+            if (supplier_code.value != '' and client_code.value != ''):
+                # Adicionar a automação aqui!
+                ...
+            else:
+                supplier_code.error_text = "Campo em branco!"
+                client_code.error_text = "Campo em branco!"
+                self.page.update()
+                sleep(3)
+                supplier_code.error_text = None
+                client_code.error_text = None
+                self.page.update()
 
         # Components
         client_code = ft.TextField(
@@ -41,6 +66,7 @@ class App:
             style=ft.ButtonStyle(
                 shape=ft.RoundedRectangleBorder(radius=5)
             ),
+            on_click=start
         )
         restart_button = ft.ElevatedButton(
             text="Reiniciar",
@@ -59,6 +85,7 @@ class App:
             width=50,
             icon_size=30,
             tooltip='Busca os Códigos no Banco de Dados',
+            on_click=search,
         )
         switch = ft.Switch(
             value=True,
