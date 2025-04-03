0fba8ebb6514ccfb6616c07b0ffa9184cb94e3c8
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-04-03 09:33:05 -0300

    update

--- src/utils/commands.py
+++ src/utils/commands.py
@@ -16,6 +16,18 @@ def smart_click(image_path: str, flag_path: str = None):
         sleep(0.5)
 
 
+def smart_click_position(x: int, y: int, flag_path: str):
+    while True:
+        try:
+            if locateOnScreen(flag_path):
+                click(x=x, y=y)
+                break
+
+        except ImageNotFoundException:
+            print('Referência não foi encontrada!')
+        sleep(0.5)
+
+
 def smart_press(image_path: str, key: str):
     while True:
         try:
